fdf988f391c85bed5d7ac01c1a13143aae66a1b0
PakinWasu <pakin.wasu2002@gmail.com>
2023-07-04 02:11:33 +0700

    test

--- Lecture01/testgit.py
+++ Lecture01/testgit.py
@@ -1,2 +1,19 @@
 print("Hello World")
 print("INE-INET Class")
+name = str(input("Enter Your Name : "))
+print("6606022420028")
+h = int(input("Enter Your h : "))
+g = int(input("Enter Your g : "))
+
+
+def hello(name):
+    print("Hello %s"% name)
+
+hello(name)
+
+def area(h,g):
+    a = (h*g)/2
+    return a
+
+print("area = %d "% area(h,g))
+print(name.title())
