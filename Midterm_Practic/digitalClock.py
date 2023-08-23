84767a145caf96efcec247c33305886614cc4cec
Pakin Wasu <138424410+PakinWasu@users.noreply.github.com>
2023-08-23 14:26:56 +0700

    Update digitalClock.py

--- Midterm_Practic/digitalClock.py
+++ Midterm_Practic/digitalClock.py
@@ -2,7 +2,19 @@ def digitalClock(sec):
     h = sec//3600
     m = (sec-(3600*h))//60
     s = (sec-(3600*h+m*60))
-    print(sec,'seconds is',h,'hours,',m,'mins, ',s,'secs.')
+    if h//24 >=1:
+      h = h%24
+    formatted_time = "{:02d}:{:02d}:{:02d}".format(h, m, s)
+    return print(formatted_time)
+
+    
 
 
 print(digitalClock(5025))
+print(digitalClock(61201))
+
+
+print(digitalClock(5025))
+print(digitalClock(61201))
+print(digitalClock(87000))
+print(digitalClock(176470))
