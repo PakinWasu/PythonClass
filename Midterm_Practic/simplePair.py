d201afb6649abc9282333d7a4201b8ab4ce77dfa
Pakin Wasu <138424410+PakinWasu@users.noreply.github.com>
2023-08-23 14:27:31 +0700

    Update simplePair.py

--- Midterm_Practic/simplePair.py
+++ Midterm_Practic/simplePair.py
@@ -1,11 +1,12 @@
-# def simplePair(num,re):
-#     for i in range(1,len(num)+1):
-#         for j in num:
-#             if j != i:
-#                 print(j)
-        
-
-
-# print(simplePair([1,2,3],3))
-# print(simplePair([1,2,3],6))
-# print(simplePair([1,2,3],9))
+def simplePair(num,re):
+  for i in range(1,len(num)+1):
+    for j in num:
+      if j != i:
+        #print(i,j)
+        res = i*j
+        if res == re:
+            return [i,j]
+  return "null"
+print(simplePair([1,2,3],3))
+print(simplePair([1,2,3],6))
+print(simplePair([1,2,3],9))
