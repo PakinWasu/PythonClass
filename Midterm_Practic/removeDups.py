83f86838b552881a0cb123a281fc04482d505f27
Pakin Wasu <138424410+PakinWasu@users.noreply.github.com>
2023-08-23 14:29:10 +0700

    Update removeDups.py

--- Midterm_Practic/removeDups.py
+++ Midterm_Practic/removeDups.py
@@ -1,9 +1,7 @@
-# def removeDups(string):
-#     print(string)
-#     for  i in range(len(string)):
-#         print(i)
-#         for j in string:
-#             if j == j:
+def removeDups(dups):
 
+    return list(set(dups))
 
-# removeDups([1,0,1,0])  
+print(removeDups([1, 0, 1, 0]))
+print(removeDups(["The", "big", "cat"]))
+print(removeDups(["John", "Taylor", "John"]))
