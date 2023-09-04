97c01bc107dc3a074307331ed1344602f295a38a
Pakin Wasu <138424410+PakinWasu@users.noreply.github.com>
2023-09-04 02:20:30 +0000

    test vs

--- Midterm_Practic/numberSplit.py
+++ Midterm_Practic/numberSplit.py
@@ -17,10 +17,10 @@ print(numberSplit(11))
 print(numberSplit(-9))
 
 #เฉลย
-def numberSplit (num):
-    return [num//2,num//2+num%2]
+# def numberSplit (num):
+#     return [num//2,num//2+num%2]
 
-print(numberSplit(4))
-print(numberSplit(10))
-print(numberSplit(11))
-print(numberSplit(-9))
+# print(numberSplit(4))
+# print(numberSplit(10))
+# print(numberSplit(11))
+# print(numberSplit(-9))
