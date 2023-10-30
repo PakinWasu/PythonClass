7010680b23b0cbd7fb0003aed7e07e3f958e8088
PakinWasu <pakin.wasu2002@gmai.com>
2023-10-31 00:40:58 +0700

    ิืิnone

--- finals_Practic/test2_eng.py
+++ finals_Practic/test2_eng.py
@@ -1,12 +1,9 @@
 def con_eng(*num):
-    #print(num)
     msg = ''
     eng = {}
     for i in range(1,26+1):
         eng.update({i:chr(64+i)})
-    #print(eng)
     for  k in num:
-        #print(k)
         msg += eng[k]
     return msg
 print(con_eng(1,14,20))#output: ANT
